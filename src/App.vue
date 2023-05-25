10ef5444ba8fd2b7947cd049cd9b78ca29c96d6d
SilectMystic <cvasquez@brooklynsteamcenter.org>
2023-05-25 14:56:34 -0400

    more code

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,19 @@ import {ref, watch} from 'vue';
 </script>
 
 <template>
-<p>hapa</p>
+<head>
+    <link rel="icon" href="../photos/SHC.png">
+</head>
+<img src="../photos/SHC.png" alt="">
+<br>
+SHC App
 </template>
 
 <style>
-
+ body {
+    background-color: #0f0e17;
+    color: white;
+    font-family: 'Nunito', sans-serif;
+    text-align: center;
+  }
 </style>
